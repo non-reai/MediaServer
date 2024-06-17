92a14c4e4b67cb4bea0645073ede5a7afe63cb75
Non_Reai <63729314+non-reai@users.noreply.github.com>
2024-06-17 22:47:44 +0000

    added more feedback for users

--- public/index.js
+++ public/index.js
@@ -2,20 +2,20 @@ const $ = (query)=>{
 	return document.querySelector(query)
 }
 
-$("#upload").addEventListener('click', ()=>{
+$("#upload").addEventListener('click', async ()=>{
 	const files = $("#files").files
 	for (let i = 0; i < files.length; i++) {
 		const file = files[i]
 		const form = new FormData
 
 		form.append("file", file)
-
-		console.log(form.get("file"))
 		
-		fetch("/upload", {
+		await fetch("/upload", {
 			method: "POST",
 			body: form,
 		})
+
+		alert("Uploaded "+file.name+" ("+(i+1)+"/"+files.length+")")
 	}
 })
 
