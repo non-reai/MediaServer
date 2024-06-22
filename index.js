26521327495fa038e1456fe99411bb132518c4a8
Non_Reai <63729314+non-reai@users.noreply.github.com>
2024-06-22 01:23:06 +0000

    styled upload button and added compression.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import fs from 'fs'
+import sharp from 'sharp'
 import formidable from 'express-formidable'
 
 const app = express()
@@ -16,7 +17,26 @@ app.get('/files', (req, res)=>{
 	res.end(JSON.stringify(fs.readdirSync('files')))
 })
 
-app.use('/files', express.static('files'))
+app.use('/files/:fileName', async (req, res)=>{
+	if (req.query.compress) {
+		const image = sharp("files/"+req.params.fileName)
+		const metadata = await image.metadata()
+		let buffer
+		
+		if (metadata.width > 2000 || metadata.height > 2000) {
+			buffer = await image.resize(Math.floor(metadata.width / 10), Math.floor(metadata.height / 10)).withMetadata().png().toBuffer()
+		} else {
+			buffer = await image.png().withMetadata().toBuffer()
+		}
+		
+		res.set('content-type', 'image/png')
+		res.end(buffer)
+	} else {
+		const buffer = fs.readFileSync("files/"+req.params.fileName)
+		res.set('content-type', 'image/png')
+		res.end(buffer)
+	}
+})
 
 app.use(express.static('public'))
 
--- public/index.js
+++ public/index.js
@@ -17,19 +17,24 @@ $("#upload").addEventListener('click', async ()=>{
 
 		alert("Uploaded "+file.name+" ("+(i+1)+"/"+files.length+")")
 	}
+	getFiles()
 })
 
 async function getFiles() {
+	while ($("#files-section").firstChild) {
+		$("#files-section").firstChild.remove()
+	}
+	
 	const response = await fetch("/files")
 	const files = await response.json()
 
-	const imageExtensions = ["png", "jpg", "jpeg"]
-
+	const imageExtensions = ["png", "jpg", "jpeg", "gif"]
+	
 	files.forEach((file)=>{
 		console.log(file)
 		if (imageExtensions.includes(file.split(".").pop())) {
 			const image = document.createElement("img")
-			image.src = "/files/"+file
+			image.src = "/files/"+file+"?compress=true"
 			$("#files-section").appendChild(image)
 			return
 		}
